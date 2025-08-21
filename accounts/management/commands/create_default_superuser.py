b2ef0a20b5d0f91d349e37d1cbb05aec762e6fb4
Adewole2016 <kunleadewole2008@gmail.com>
2025-08-21 19:52:56 +0100

    Update create_default_superuser.py

--- accounts/management/commands/create_default_superuser.py
+++ accounts/management/commands/create_default_superuser.py
@@ -1,15 +1,13 @@
 from django.core.management.base import BaseCommand
-from django.core.management import call_command
 from django.contrib.auth import get_user_model
 from django.conf import settings
-import os
 
 class Command(BaseCommand):
-    help = 'Create a default superuser and load users from fixture'
+    help = 'Create a default superuser with role admin if it does not exist'
 
     def handle(self, *args, **kwargs):
-        # Step 1: Create default superuser if it does not exist
         User = get_user_model()
+
         username = getattr(settings, 'DJANGO_SUPERUSER_USERNAME', 'admin')
         email = getattr(settings, 'DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
         password = getattr(settings, 'DJANGO_SUPERUSER_PASSWORD', 'admin')
@@ -19,13 +17,3 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created successfully.'))
         else:
             self.stdout.write(self.style.WARNING(f'Superuser "{username}" already exists.'))
-
-        # Step 2: Load users from fixture
-        fixture_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'fixtures', 'users.json')
-        if os.path.exists(fixture_path):
-            call_command('loaddata', fixture_path)
-            self.stdout.write(self.style.SUCCESS("Users loaded successfully from users.json."))
-        else:
-            self.stdout.write(self.style.WARNING(f"Fixture file not found at {fixture_path}"))
-
-
